6fc572a8437c6627f27f5d4237700abc98f980c6
Yun Feng <131803211+like45599@users.noreply.github.com>
2024-02-26 22:47:33 +0800

    添加跨域配置文件注释

--- src/main/java/com/yupi/moonBI/config/WebMvcConfig.java
+++ src/main/java/com/yupi/moonBI/config/WebMvcConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 允许跨域配置类
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
