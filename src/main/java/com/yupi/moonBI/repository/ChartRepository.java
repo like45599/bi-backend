399773d9cf68104f7049b0d2b69aeddd72172cf1
Yun Feng <131803211+like45599@users.noreply.github.com>
2024-02-29 20:28:39 +0800

    Update ChartRepository.java

--- src/main/java/com/yupi/moonBI/repository/ChartRepository.java
+++ src/main/java/com/yupi/moonBI/repository/ChartRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
